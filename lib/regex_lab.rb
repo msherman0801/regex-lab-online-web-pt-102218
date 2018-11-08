429160e9251b2deaa346e93bf20db7dc6f5c6a8a
Matthew Sherman <msherman0801@gmail.com>
2018-11-08 01:38:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,4 +9,11 @@ end
 def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
 end
-#
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match(/\A[A-Z]/) && text.match(/[!?.]\z/) ? true : false
+end
+
+def valid_phone_number?(phone)
+  phone.match(/^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/) ? true : false
+end
